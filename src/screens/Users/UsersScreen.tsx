56791366644271a65d3d2d6cbb0cf35f3ac9ed4e
=sairam <=satya.sai@zatohq.com>
2026-05-28 15:14:11 +0530

    send invitation

--- src/screens/Users/UsersScreen.tsx
+++ src/screens/Users/UsersScreen.tsx
@@ -1,170 +1,397 @@
-
 import React, { useState, useEffect, useRef } from 'react';
-import { Table, Button, Space, Modal, Form, Input, message, Popconfirm } from 'antd';
-import { EditOutlined, DeleteOutlined, PlusOutlined } from '@ant-design/icons';
+
+import {
+	Table,
+	Button,
+	Space,
+	Modal,
+	Form,
+	Input,
+	message,
+	Popconfirm
+} from 'antd';
+
+import {
+	EditOutlined,
+	DeleteOutlined,
+	PlusOutlined,
+	MailOutlined
+} from '@ant-design/icons';
+
 import { useNavigate } from 'react-router-dom';
+
 import { useDispatch, useSelector } from 'react-redux';
-import { fetchUsers, addUser, updateUser, deleteUser, type User } from './usersSlice';
+
+import {
+	fetchUsers,
+	addUser,
+	updateUser,
+	deleteUser,
+	sendInvitation,
+	type User
+} from './usersSlice';
+
 import type { RootState, AppDispatch } from '../../store';
 
+
 const UsersScreen: React.FC = () => {
-  const navigate = useNavigate();
-  const dispatch = useDispatch<AppDispatch>();
-  const { users, fetchLoading, addLoading, updateLoading, addError, updateError, deleteError } = useSelector((state: RootState) => state.users);
 
-  const [isModalOpen, setIsModalOpen] = useState(false);
-  const [isEdit, setIsEdit] = useState(false);
-  const [editingUser, setEditingUser] = useState<User | null>(null);
-  const [form] = Form.useForm();
-  
-  const prevAddLoadingRef = useRef(true);
-  const prevUpdateLoadingRef = useRef(true);
+	const navigate = useNavigate();
 
-  useEffect(() => {
-    dispatch(fetchUsers());
-  }, [dispatch]);
+	const dispatch = useDispatch<AppDispatch>();
 
-  useEffect(() => {
-    if (addError) message.error(addError);
-  }, [addError]);
+	const {
+		users,
+		fetchLoading,
+		addLoading,
+		updateLoading,
+		inviteLoading,
+		addError,
+		updateError,
+		deleteError,
+		inviteError
+	} = useSelector((state: RootState) => state.users);
 
-  useEffect(() => {
-    if (updateError) message.error(updateError);
-  }, [updateError]);
 
-  useEffect(() => {
-    if (deleteError) message.error(deleteError);
-  }, [deleteError]);
+	const [isModalOpen, setIsModalOpen] = useState(false);
 
-  useEffect(() => {
-    if (prevAddLoadingRef.current && !addLoading && isModalOpen && !addError && !isEdit) {
-      setIsModalOpen(false);
-      form.resetFields();
-      message.success('User added successfully!');
-    }
-    prevAddLoadingRef.current = addLoading;
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [addLoading]);
+	const [isEdit, setIsEdit] = useState(false);
 
-  useEffect(() => {
-    if (prevUpdateLoadingRef.current && !updateLoading && isModalOpen && !updateError && isEdit) {
-      setIsModalOpen(false);
-      setIsEdit(false);
-      setEditingUser(null);
-      form.resetFields();
-      message.success('User updated successfully!');
-    }
-    prevUpdateLoadingRef.current = updateLoading;
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [updateLoading]);
+	const [editingUser, setEditingUser] = useState<User | null>(null);
 
-  const getColumns = () => [
-    {
-      title: 'ID',
-      dataIndex: 'id',
-      key: 'id',
-    },
-    {
-      title: 'Name',
-      dataIndex: 'name',
-      key: 'name',
-    },
-    {
-      title: 'Email',
-      dataIndex: 'email',
-      key: 'email',
-    },
-    {
-      title: 'Actions',
-      key: 'actions',
-      render: (_: React.ReactNode, record: User) => (
-        <Space>
-          <Button icon={<EditOutlined />} type="link" onClick={() => handleEdit(record)} />
-          <Popconfirm title="Delete user?" onConfirm={() => handleDelete(record.id)} okText="Yes" cancelText="No">
-            <Button icon={<DeleteOutlined />} type="link" danger />
-          </Popconfirm>
-        </Space>
-      ),
-    },
-  ];
+	const [form] = Form.useForm();
 
-  const showModal = () => {
-    setIsEdit(false);
-    setEditingUser(null);
-    setIsModalOpen(true);
-    form.resetFields();
-  };
+	const prevAddLoadingRef = useRef(true);
 
-  const handleEdit = (user: User) => {
-    setIsEdit(true);
-    setEditingUser(user);
-    setIsModalOpen(true);
-    form.resetFields();
-    setTimeout(() => {
-      form.setFieldsValue({ name: user.name, email: user.email });
-    }, 0);
-  };
+	const prevUpdateLoadingRef = useRef(true);
 
-  const handleDelete = (id: number) => {
-    dispatch(deleteUser(id));
-  };
 
-  const handleCancel = () => {
-    setIsModalOpen(false);
-    setIsEdit(false);
-    setEditingUser(null);
-    form.resetFields();
-  };
+	useEffect(() => {
+		dispatch(fetchUsers());
+	}, [dispatch]);
 
-  const handleOk = () => {
-    form.validateFields().then(values => {
-      if (isEdit && editingUser) {
-        dispatch(updateUser({ id: editingUser.id, data: values }));
-      } else {
-        dispatch(addUser(values));
-      }
-    });
-  };
 
-  return (
-    <div className="p-4">
-      <Table
-        columns={getColumns()}
-        dataSource={users}
-        rowKey="id"
-        loading={fetchLoading}
-        pagination={false}
-      />
-      <div className="flex justify-between mt-4">
-        <Button type="primary" icon={<PlusOutlined />} onClick={showModal} loading={addLoading}>Add User</Button>
-        <Button type="default" className="ml-2" onClick={() => navigate('/iteams')}>
-          Iteams
-        </Button>
-      </div>
-      <Modal
-        title={isEdit ? 'Edit User' : 'Add User'}
-        open={isModalOpen}
-        onOk={handleOk}
-        onCancel={handleCancel}
-        okText={isEdit ? 'Update' : 'Add'}
-        confirmLoading={addLoading || updateLoading}
-      >
-        <Form form={form} layout="vertical">
-          <Form.Item name="name" label="Name" rules={[{ required: true, message: 'Please enter name' }]}> 
-            <Input placeholder="Enter user name" /> 
-          </Form.Item>
-          <Form.Item name="email" label="Email" rules={[{ required: true, message: 'Please enter email' }, { type: 'email', message: 'Invalid email format' }]}> 
-            <Input type="email" placeholder="Enter user email" /> 
-          </Form.Item>
-          {(addError || updateError) && (
-            <div style={{ color: 'red', marginTop: 8 }}>
-              {addError || updateError}
-            </div>
-          )}
-        </Form>
-      </Modal>
-    </div>
-  );
+	useEffect(() => {
+		if (addError) message.error(addError);
+	}, [addError]);
+
+
+	useEffect(() => {
+		if (updateError) message.error(updateError);
+	}, [updateError]);
+
+
+	useEffect(() => {
+		if (deleteError) message.error(deleteError);
+	}, [deleteError]);
+
+
+	useEffect(() => {
+		if (inviteError) {
+			message.error(inviteError);
+		}
+	}, [inviteError]);
+
+
+	useEffect(() => {
+
+		if (
+			prevAddLoadingRef.current &&
+			!addLoading &&
+			isModalOpen &&
+			!addError &&
+			!isEdit
+		) {
+			setIsModalOpen(false);
+
+			form.resetFields();
+
+			message.success('User added successfully!');
+		}
+
+		prevAddLoadingRef.current = addLoading;
+
+	}, [addLoading]);
+
+
+	useEffect(() => {
+
+		if (
+			prevUpdateLoadingRef.current &&
+			!updateLoading &&
+			isModalOpen &&
+			!updateError &&
+			isEdit
+		) {
+			setIsModalOpen(false);
+
+			setIsEdit(false);
+
+			setEditingUser(null);
+
+			form.resetFields();
+
+			message.success('User updated successfully!');
+		}
+
+		prevUpdateLoadingRef.current = updateLoading;
+
+	}, [updateLoading]);
+
+
+	const handleSendInvitation = async (user: User) => {
+
+		try {
+
+			await dispatch(
+				sendInvitation({
+					name: user.name,
+					email: user.email,
+				})
+			).unwrap();
+
+			message.success(
+				`Invitation sent to ${user.email}`
+			);
+
+		} catch (error) {
+
+			console.error(error);
+
+			message.error('Failed to send invitation');
+		}
+	};
+
+
+	const getColumns = () => [
+
+		{
+			title: 'ID',
+			dataIndex: 'id',
+			key: 'id',
+		},
+
+		{
+			title: 'Name',
+			dataIndex: 'name',
+			key: 'name',
+		},
+
+		{
+			title: 'Email',
+			dataIndex: 'email',
+			key: 'email',
+		},
+
+		{
+			title: 'Actions',
+			key: 'actions',
+
+			render: (_: React.ReactNode, record: User) => (
+
+				<Space>
+
+					<Button
+						icon={<MailOutlined />}
+						type="link"
+						onClick={() => handleSendInvitation(record)}
+						loading={inviteLoading}
+					/>
+
+					<Button
+						icon={<EditOutlined />}
+						type="link"
+						onClick={() => handleEdit(record)}
+					/>
+
+					<Popconfirm
+						title="Delete user?"
+						onConfirm={() => handleDelete(record.id)}
+						okText="Yes"
+						cancelText="No"
+					>
+						<Button
+							icon={<DeleteOutlined />}
+							type="link"
+							danger
+						/>
+					</Popconfirm>
+
+				</Space>
+			),
+		},
+	];
+
+
+	const showModal = () => {
+
+		setIsEdit(false);
+
+		setEditingUser(null);
+
+		setIsModalOpen(true);
+
+		form.resetFields();
+	};
+
+
+	const handleEdit = (user: User) => {
+
+		setIsEdit(true);
+
+		setEditingUser(user);
+
+		setIsModalOpen(true);
+
+		form.resetFields();
+
+		setTimeout(() => {
+
+			form.setFieldsValue({
+				name: user.name,
+				email: user.email,
+			});
+
+		}, 0);
+	};
+
+
+	const handleDelete = (id: number) => {
+		dispatch(deleteUser(id));
+	};
+
+
+	const handleCancel = () => {
+
+		setIsModalOpen(false);
+
+		setIsEdit(false);
+
+		setEditingUser(null);
+
+		form.resetFields();
+	};
+
+
+	const handleOk = () => {
+
+		form.validateFields().then(values => {
+
+			if (isEdit && editingUser) {
+
+				dispatch(
+					updateUser({
+						id: editingUser.id,
+						data: values,
+					})
+				);
+
+			} else {
+
+				dispatch(addUser(values));
+			}
+		});
+	};
+
+
+	return (
+
+		<div className="p-4">
+
+			<Table
+				columns={getColumns()}
+				dataSource={users}
+				rowKey="id"
+				loading={fetchLoading}
+				pagination={false}
+			/>
+
+			<div className="flex justify-between mt-4">
+
+				<Button
+					type="primary"
+					icon={<PlusOutlined />}
+					onClick={showModal}
+					loading={addLoading}
+				>
+					Add User
+				</Button>
+
+				<Button
+					type="default"
+					className="ml-2"
+					onClick={() => navigate('/iteams')}
+				>
+					Iteams
+				</Button>
+
+			</div>
+
+
+			<Modal
+				title={isEdit ? 'Edit User' : 'Add User'}
+				open={isModalOpen}
+				onOk={handleOk}
+				onCancel={handleCancel}
+				okText={isEdit ? 'Update' : 'Add'}
+				confirmLoading={addLoading || updateLoading}
+			>
+
+				<Form
+					form={form}
+					layout="vertical"
+				>
+
+					<Form.Item
+						name="name"
+						label="Name"
+						rules={[
+							{
+								required: true,
+								message: 'Please enter name'
+							}
+						]}
+					>
+						<Input placeholder="Enter user name" />
+					</Form.Item>
+
+					<Form.Item
+						name="email"
+						label="Email"
+						rules={[
+							{
+								required: true,
+								message: 'Please enter email'
+							},
+							{
+								type: 'email',
+								message: 'Invalid email format'
+							}
+						]}
+					>
+						<Input
+							type="email"
+							placeholder="Enter user email"
+						/>
+					</Form.Item>
+
+					{(addError || updateError) && (
+						<div
+							style={{
+								color: 'red',
+								marginTop: 8
+							}}
+						>
+							{addError || updateError}
+						</div>
+					)}
+
+				</Form>
+
+			</Modal>
+
+		</div>
+	);
 };
 
 export default UsersScreen;
--- src/screens/Users/usersSlice.ts
+++ src/screens/Users/usersSlice.ts
@@ -1,4 +1,3 @@
-
 import { createSlice, createAsyncThunk, type PayloadAction } from '@reduxjs/toolkit';
 
 import axios from 'axios';
@@ -23,135 +22,253 @@ interface UsersState {
 	users: User[];
 	fetchLoading: boolean;
 	fetchError: string | null;
+
 	addLoading: boolean;
 	addError: string | null;
+
 	updateLoading: boolean;
 	updateError: string | null;
+
 	deleteLoading: boolean;
 	deleteError: string | null;
+
+	inviteLoading: boolean;
+	inviteError: string | null;
 }
 
 const initialState: UsersState = {
 	users: [],
+
 	fetchLoading: false,
 	fetchError: null,
+
 	addLoading: false,
 	addError: null,
+
 	updateLoading: false,
 	updateError: null,
+
 	deleteLoading: false,
 	deleteError: null,
+
+	inviteLoading: false,
+	inviteError: null,
 };
 
 const API_URL = 'https://fastapi-backend-89xk.onrender.com/users';
 
-// Thunks
-export const fetchUsers = createAsyncThunk('users/fetchUsers', async (_, { rejectWithValue }) => {
-	try {
-		const res = await axios.get<User[]>(API_URL);
-		return res.data;
-	} catch (err: unknown) {
-		if (axios.isAxiosError(err)) {
-			return rejectWithValue(err.response?.data?.detail || 'Failed to fetch users');
-		}
-		return rejectWithValue('Failed to fetch users');
-	}
-});
 
-export const addUser = createAsyncThunk('users/addUser', async (user: UserCreate, { rejectWithValue }) => {
-	try {
-		const res = await axios.post<User>(API_URL + '/', user);
-		return res.data;
-	} catch (err: unknown) {
-		if (axios.isAxiosError(err)) {
-			return rejectWithValue(err.response?.data?.detail || 'Failed to add user');
+// FETCH USERS
+export const fetchUsers = createAsyncThunk(
+	'users/fetchUsers',
+	async (_, { rejectWithValue }) => {
+		try {
+			const res = await axios.get<User[]>(API_URL);
+			return res.data;
+		} catch (err: unknown) {
+			if (axios.isAxiosError(err)) {
+				return rejectWithValue(
+					err.response?.data?.detail || 'Failed to fetch users'
+				);
+			}
+			return rejectWithValue('Failed to fetch users');
 		}
-		return rejectWithValue('Failed to add user');
 	}
-});
+);
 
-export const updateUser = createAsyncThunk('users/updateUser', async ({ id, data }: { id: number; data: UserUpdate }, { rejectWithValue }) => {
-	try {
-		const res = await axios.put<User>(`${API_URL}/${id}`, data);
-		return res.data;
-	} catch (err: unknown) {
-		if (axios.isAxiosError(err)) {
-			return rejectWithValue(err.response?.data?.detail || 'Failed to update user');
-		}
-		return rejectWithValue('Failed to update user');
-	}
-});
 
-export const deleteUser = createAsyncThunk('users/deleteUser', async (id: number, { rejectWithValue }) => {
-	try {
-		await axios.delete(`${API_URL}/${id}`);
-		return id;
-	} catch (err: unknown) {
-		if (axios.isAxiosError(err)) {
-			return rejectWithValue(err.response?.data?.detail || 'Failed to delete user');
+// ADD USER
+export const addUser = createAsyncThunk(
+	'users/addUser',
+	async (user: UserCreate, { rejectWithValue }) => {
+		try {
+			const res = await axios.post<User>(API_URL + '/', user);
+			return res.data;
+		} catch (err: unknown) {
+			if (axios.isAxiosError(err)) {
+				return rejectWithValue(
+					err.response?.data?.detail || 'Failed to add user'
+				);
+			}
+			return rejectWithValue('Failed to add user');
 		}
-		return rejectWithValue('Failed to delete user');
 	}
-});
+);
+
+
+// UPDATE USER
+export const updateUser = createAsyncThunk(
+	'users/updateUser',
+	async (
+		{ id, data }: { id: number; data: UserUpdate },
+		{ rejectWithValue }
+	) => {
+		try {
+			const res = await axios.put<User>(
+				`${API_URL}/${id}`,
+				data
+			);
+
+			return res.data;
+		} catch (err: unknown) {
+			if (axios.isAxiosError(err)) {
+				return rejectWithValue(
+					err.response?.data?.detail || 'Failed to update user'
+				);
+			}
+			return rejectWithValue('Failed to update user');
+		}
+	}
+);
+
+
+// DELETE USER
+export const deleteUser = createAsyncThunk(
+	'users/deleteUser',
+	async (id: number, { rejectWithValue }) => {
+		try {
+			await axios.delete(`${API_URL}/${id}`);
+			return id;
+		} catch (err: unknown) {
+			if (axios.isAxiosError(err)) {
+				return rejectWithValue(
+					err.response?.data?.detail || 'Failed to delete user'
+				);
+			}
+			return rejectWithValue('Failed to delete user');
+		}
+	}
+);
+
+
+// SEND INVITATION
+export const sendInvitation = createAsyncThunk(
+	'users/sendInvitation',
+	async (
+		{ name, email }: { name: string; email: string },
+		{ rejectWithValue }
+	) => {
+		try {
+			const res = await axios.post(
+				`${API_URL}/invite`,
+				{
+					name,
+					email,
+				}
+			);
+
+			return res.data;
+		} catch (err: unknown) {
+			if (axios.isAxiosError(err)) {
+				return rejectWithValue(
+					err.response?.data?.detail || 'Failed to send invitation'
+				);
+			}
+			return rejectWithValue('Failed to send invitation');
+		}
+	}
+);
+
 
 const usersSlice = createSlice({
 	name: 'users',
 	initialState,
 	reducers: {},
 	extraReducers: (builder) => {
+
 		builder
-			// Fetch
+
+			// FETCH
 			.addCase(fetchUsers.pending, (state) => {
 				state.fetchLoading = true;
 				state.fetchError = null;
 			})
+
 			.addCase(fetchUsers.fulfilled, (state, action: PayloadAction<User[]>) => {
 				state.fetchLoading = false;
 				state.users = action.payload;
 			})
+
 			.addCase(fetchUsers.rejected, (state, action) => {
 				state.fetchLoading = false;
 				state.fetchError = action.payload as string;
 			})
-			// Add
+
+
+			// ADD
 			.addCase(addUser.pending, (state) => {
 				state.addLoading = true;
 				state.addError = null;
 			})
+
 			.addCase(addUser.fulfilled, (state, action: PayloadAction<User>) => {
 				state.addLoading = false;
 				state.users.push(action.payload);
 			})
+
 			.addCase(addUser.rejected, (state, action) => {
 				state.addLoading = false;
 				state.addError = action.payload as string;
 			})
-			// Update
+
+
+			// UPDATE
 			.addCase(updateUser.pending, (state) => {
 				state.updateLoading = true;
 				state.updateError = null;
 			})
+
 			.addCase(updateUser.fulfilled, (state, action: PayloadAction<User>) => {
 				state.updateLoading = false;
-				const idx = state.users.findIndex(u => u.id === action.payload.id);
-				if (idx !== -1) state.users[idx] = action.payload;
+
+				const idx = state.users.findIndex(
+					u => u.id === action.payload.id
+				);
+
+				if (idx !== -1) {
+					state.users[idx] = action.payload;
+				}
 			})
+
 			.addCase(updateUser.rejected, (state, action) => {
 				state.updateLoading = false;
 				state.updateError = action.payload as string;
 			})
-			// Delete
+
+
+			// DELETE
 			.addCase(deleteUser.pending, (state) => {
 				state.deleteLoading = true;
 				state.deleteError = null;
 			})
+
 			.addCase(deleteUser.fulfilled, (state, action: PayloadAction<number>) => {
 				state.deleteLoading = false;
-				state.users = state.users.filter(u => u.id !== action.payload);
+
+				state.users = state.users.filter(
+					u => u.id !== action.payload
+				);
 			})
+
 			.addCase(deleteUser.rejected, (state, action) => {
 				state.deleteLoading = false;
 				state.deleteError = action.payload as string;
+			})
+
+
+			// INVITATION
+			.addCase(sendInvitation.pending, (state) => {
+				state.inviteLoading = true;
+				state.inviteError = null;
+			})
+
+			.addCase(sendInvitation.fulfilled, (state) => {
+				state.inviteLoading = false;
+			})
+
+			.addCase(sendInvitation.rejected, (state, action) => {
+				state.inviteLoading = false;
+				state.inviteError = action.payload as string;
 			});
 	},
 });
